12b2aa4b4ff78d390cfeb8547ad206771d702cd1
Liem Mazal <72171292+liem1996@users.noreply.github.com>
2020-12-23 08:39:35 +0200

    Update index.js

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -46,26 +46,6 @@ function ComputerChoise() {
 
   UserCohise();
 
-  if (compChoise===1)
-  {
-    document.getElementById("computer_select").querySelector("img").src = "./assets/images/icon-paper.svg";
-    document.getElementById("computer_select").classList.add('btn-paper');
-    compChoise = "paper";
-  }
-  if (compChoise ===2)
-  {
-    document.getElementById("computer_select").querySelector("img").src = "./assets/images/icon-scissors.svg";
-    document.getElementById("computer_select").classList.add('btn-scissors');
-    compChoise ="scissors";
-  }
-  if (compChoise ===0)
-  {
-    document.getElementById("computer_select").querySelector("img").src = "./assets/images/icon-rock.svg";
-    document.getElementById("computer_select").classList.add('btn-rock');
-    compChoise ="rock";
-  }
-  return compChoise;
-}
 
 function updateScore(value) {
   score = score + value;
